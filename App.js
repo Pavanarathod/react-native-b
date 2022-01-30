82aa0e44a76d7c3c0d08ba56a345b7e31e6c4f57
Pavanarathod <edrocullen13@gmail.com>
2022-01-30 21:07:58 +0530

    learning core components

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [goalItems, setGoalItems] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const handleGoal = () => {
+    setGoalItems([...goalItems, enteredGoal]);
+  };
+
+  const remobeGoal = (goalItem) => {
+    const fillteredItem = goalItems.filter((item) => item !== goalItem);
+    setGoalItems(fillteredItem);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>I ma coming for you Abhishek jn</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.mainHeader}>Set you'r Goals.</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+          placeholder="Course Goal"
+          style={styles.textInput}
+        />
+        <Button onPress={handleGoal} title="Add you'r goal" />
+      </View>
+      <FlatList
+        data={goalItems}
+        renderItem={(item) => (
+          <Text onPress={() => remobeGoal(item)} style={styles.textCard}>
+            {item.item}
+          </Text>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
+    height: "100%",
+    backgroundColor: "whitesmoke",
+    padding: 20,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
+  },
+  textInput: {
+    borderWidth: 1,
+    padding: 5,
+    flex: 1,
+  },
+  mainHeader: {
+    fontSize: 40,
+    fontWeight: "600",
+    textAlign: "center",
+    marginBottom: 10,
+    marginTop: 10,
+  },
+  cardContainer: {
+    marginTop: 10,
+  },
+  textCard: {
+    backgroundColor: "#FFFF",
+    paddingLeft: 10,
+    padding: 10,
+    marginTop: 10,
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 50,
   },
 });
